a6585efad9f3e38d0a336f1714e84215227dcb25
dantezinfern0 <danteharasz@gmail.com>
2019-08-09 15:19:50 -0400

    extra credit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import DisplayComponent from './DisplayComponent.jsx'
+import Scout from './Scout.js'
 const _Scout = window.Scout
 
 class Search extends Component {
@@ -9,62 +9,20 @@ class Search extends Component {
     soloData: 'p2.br.m0.weekly',
     duoData: 'p10.br.m0.weekly',
     squadsData: 'p9.br.m0.weekly',
-    searchData: [],
-    titleSearch: '',
-    killDeathRatioSearch: '',
-    matchesSearch: '',
-    winsSearch: '',
-    top12Search: '',
-    winRateSearch: 0,
+    dataType: '',
     stats: ''
   }
+  // this function is supposed to update the state with user input
   setValue = e => {
     this.setState({
       [e.target.name]: e.target.value, 
     })
     console.log(this.state.stats, this.state.playerName)
-  }
-  fetchData = async e => {
-    e.preventDefault()
-    await _Scout.configure({
-      clientId: process.env.REACT_APP_CLIENT_ID,
-      clientSecret: process.env.REACT_APP_CLIENT_SECRET,
-      scope: 'public.read'
-    })
-
-    var titles = await _Scout.titles.list()
-    var fortnite = titles.find(t => t.slug === 'fortnite')
-
-    _Scout.players
-      .search(`${this.state.playerName}`, 'epic', 'pc', fortnite.id, true, true)
-      .then(data => {
-        var playerId = data.results[0].player.playerId
-
-        _Scout.players
-          .get(fortnite.id, playerId, `${this.state.stats}`)
-          .then(data => {
-            console.log('Ajax call Done', data)
-            this.setState({
-              searchData: data,
-              titleSearch: data.segments[0].metadata[0].displayValue.split(
-                ' ',
-                1
-              ),
-              killsSearch: data.stats[0].displayValue,
-              matchesSearch: data.stats[2].displayValue,
-              winsSearch: data.stats[5].displayValue,
-              top12Search: data.stats[8].displayValue,
-              killDeathRatioSearch: data.stats[11].displayValue,
-              winRateSearch: Math.round(data.stats[12].value * 100),
-              loading: false
-            })
-          })
-      })
-  }
+  }  
   render() {
     return (
       <div>
-        <form className="form-layout" name="userChoice" onSubmit={e => this.setValue}>
+        <form className="form-layout" name="userChoice" onSubmit={this.setState({dataType:'radio button that is true'})}>
           <input name="playerName" onChange={e => this.setValue} type="text" alt="name" />
           <div>
             <input
@@ -72,6 +30,7 @@ class Search extends Component {
               id="statChoice1"
               name="stats"
               value={this.state.soloData}
+              checked={true}
             />
             <label for="statChoice1">Solo</label>
             <input
@@ -79,6 +38,7 @@ class Search extends Component {
               id="statChoice2"
               name="stats"
               value={this.state.duoData}
+              checked={true}
             />
             <label for="statChoice2">Duo</label>
             <input
@@ -86,14 +46,13 @@ class Search extends Component {
               id="statChoice3"
               name="stats"
               value={this.state.squadsData}
+              checked={true}
             />
             <label for="statChoice3">Squad</label>
           </div>
-          {/* There is something wrong here, I'm having trouble hooking up the radio- */}
-          {/* button choice to the fetch request && getting it to fetch */}
-          <button onClick={e => this.fetchData}>submit</button>
+          <button onClick={this.setState({loading: false})}>submit</button>
         </form>
-        {!this.state.loading && <DisplayComponent />}
+        {!this.state.loading && <Scout dataType={this.state.dataType} />}
       </div>
     )
   }
